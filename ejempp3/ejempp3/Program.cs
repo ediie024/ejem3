640917b1e63159ad87b0937c236adb71e471e514
Eduardo Collantes <eduardocollantes2410@gmail.com>
2020-08-31 22:45:01 -0500

    primero

---
+++ ejempp3/ejempp3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ejempp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("holaa sdasdlasd!");
+        }
+    }
+}
